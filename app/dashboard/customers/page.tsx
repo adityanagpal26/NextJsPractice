ba235a9864ef58f81d3c3110e1a9244b3206da5d
Aditya Nagpal <adityanagpal26@gmail.com>
2024-06-24 21:34:19 +0530

    first commit

---
+++ app/dashboard/customers/page.tsx
@@ -0,0 +1,9 @@
+import React from 'react'
+
+function customers() {
+  return (
+    <p>customers</p>
+  )
+}
+
+export default customers
---
+++ app/dashboard/invoices/page.tsx
@@ -0,0 +1,9 @@
+import React from 'react'
+
+function invoices() {
+  return (
+    <p>invoices</p>
+  )
+}
+
+export default invoices
